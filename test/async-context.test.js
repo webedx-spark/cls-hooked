6a9a415d03e7b4f170c5256a03734dad029e5f55
jeff-lewis <jlewis@streetconnect.com>
2017-07-25 11:51:13 -0400

    refactoring debug statements - WIP

--- test/async-context.test.js
+++ test/async-context.test.js
@@ -3,16 +3,29 @@
 const expect = require('chai').expect;
 const cls = require('../index.js');
 
-describe("cls simple async local context", function () {
+describe('cls simple async local context', function () {
+    let namespace;
 
-    it("asynchronously propagating state with local-context", function (done) {
-        var namespace = cls.createNamespace('namespace');
-        expect(process.namespaces.namespace, "namespace has been created");
+    before(function(){
+      namespace = cls.createNamespace('namespace');
+    });
+
+    it('asynchronously propagating state with local-context', function (done) {
+
+        expect(process.namespaces.namespace, 'namespace has been created');
 
         namespace.run(function () {
             namespace.set('test', 1337);
-            expect(namespace.get('test')).equal(1337, "namespace is working");
+            expect(namespace.get('test')).equal(1337, 'namespace is working');
             done();
         });
+    });
+
+    it('should destroy context pointers in _set', function (){
+        expect(namespace._set.length).equal(0, '_set cleared of contexts');
+    });
+
+    it('should destroy context pointers in _contexts', function (){
+        expect(namespace._contexts.size).equal(0, '_contexts cleared of contexts');
     });
 });
